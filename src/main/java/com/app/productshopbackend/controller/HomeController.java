d6fb64de25872622adaf944c83089c51b6a1b4a2
trieuvanson <Vodanhserver@gmail.com>
2021-07-11 13:59:59 +0700

    Commit

--- src/main/java/com/app/productshopbackend/controller/HomeController.java
+++ src/main/java/com/app/productshopbackend/controller/HomeController.java
@@ -6,13 +6,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import static jdk.nashorn.internal.objects.NativeArray.forEach;
+
 @CrossOrigin
-@Controller
+@RestController
 @RequestMapping("/")
 public class HomeController {
     @Autowired
@@ -22,9 +26,14 @@ public class HomeController {
             , "options", "orderview", "product-images", "products", "slides", "submit-contact", "users");
 
     @GetMapping("/")
-    public String Home(Model model) {
-        model.addAttribute("links", list);
-        model.addAttribute("url", request.getRequestURL().toString());
-        return "home";
+    public String Home() {
+//        model.addAttribute("links", list);
+//        model.addAttribute("url", request.getRequestURL().toString());
+        String url = request.getRequestURL().toString();
+        String body = "<tr>\n" +
+                "            <td><a href=\""+url+"swagger-ui/index.html\">swagger-ui-api</a></td>\n" +
+            "         </tr>";
+
+        return body;
     }
 }
